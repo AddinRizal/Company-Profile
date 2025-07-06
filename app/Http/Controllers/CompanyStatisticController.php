bae357ffa1842428d465295640b66fc220d2e620
AddinRizal <addinhadi10403@gmail.com>
2025-07-06 22:13:58 +0700

    request

--- app/Http/Controllers/CompanyStatisticController.php
+++ app/Http/Controllers/CompanyStatisticController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreStatisticRequest;
+use App\Http\Requests\UpdateStatisticRequest;
 use App\Models\CompanyStatistic;
+use DB;
 use Illuminate\Http\Request;
 
 class CompanyStatisticController extends Controller
@@ -13,6 +16,8 @@ class CompanyStatisticController extends Controller
     public function index()
     {
         //
+        $statistics = CompanyStatistic::orderByDesc('id')->paginate(10);
+        return view('admin.statistics.index', compact('statistics'));
     }
 
     /**
@@ -21,14 +26,29 @@ class CompanyStatisticController extends Controller
     public function create()
     {
         //
+        return view('admin.statistics.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreStatisticRequest $request)
     {
-        //
+        // insert into database 
+        // closure based transactions
+
+        DB::transaction(function () use ($request) {
+            $validated = $request->validated();
+
+            if($request->hasFile('icon')){
+                $iconPath = $request->file('icon')->store('icons', 'public');
+                $validated['icon'] = $iconPath;
+            }
+
+            $newDataRecored = CompanyStatistic::create($validated);
+        });
+
+        return redirect()->route('admin.statistics.index');
     }
 
     /**
@@ -42,24 +62,42 @@ class CompanyStatisticController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(CompanyStatistic $companyStatistic)
+    public function edit(CompanyStatistic $statistic)
     {
         //
+        return view('admin.statistics.edit', compact('statistic'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, CompanyStatistic $companyStatistic)
+    public function update(UpdateStatisticRequest $request, CompanyStatistic $statistic)
     {
         //
+        DB::transaction(function () use ($request, $statistic) {
+            $validated = $request->validated();
+
+            if($request->hasFile('icon')){
+                $iconPath = $request->file('icon')->store('icons', 'public');
+                $validated['icon'] = $iconPath;
+            }
+
+            $statistic->update($validated);
+        });
+
+        return redirect()->route('admin.statistics.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(CompanyStatistic $companyStatistic)
+    public function destroy(CompanyStatistic $statistic)
     {
         //
+        DB::transaction(function() use ($statistic){
+            $statistic->delete();
+        });
+
+        return redirect()->route('admin.statistics.index');
     }
 }

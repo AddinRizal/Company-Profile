fbaae5503bcd9dbfe9c5f39ae23ec5d10041eaf9
AddinRizal <addinhadi10403@gmail.com>
2025-07-20 18:50:30 +0700

    update error done

--- resources/views/admin/testimonials/index.blade.php
+++ resources/views/admin/testimonials/index.blade.php
@@ -13,31 +13,26 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col gap-y-5">
-                @forelse($testimonials as $testimonial)
 
+            @forelse($testimonials as $testimonial)
                 <div class="item-card flex flex-row justify-between items-center">
                     <div class="flex flex-row items-center gap-x-3">
-                        <img src="{{Storage::url($testimonial->thumbnail)}}" alt="" class="rounded-2xl object-cover w-[90px] h-[90px]">
+                        <img src="{{asset('storage/' . $testimonial->thumbnail)}} " alt="" class="rounded-2xl object-cover w-[90px] h-[90px]">
                         <div class="flex flex-col">
-                            <h3 class="text-indigo-950 text-xl font-bold">
-                                {{$testimonial->projectClient->name}}
-                            </h3>
+                            <h3 class="text-indigo-950 text-xl font-bold">{{$testimonial->message}}</h3>
                         </div>
                     </div> 
                     <div  class="hidden md:flex flex-col">
                         <p class="text-slate-500 text-sm">Date</p>
-                        <h3 class="text-indigo-950 text-xl font-bold">
-                            {{$testimonial->created_at}}
-                        </h3>
+                        <h3 class="text-indigo-950 text-xl font-bold">{{$testimonial->created_at->format('M d, Y')}}</h3>
                     </div>
                     <div class="hidden md:flex flex-row items-center gap-x-3">
-                        <a href="{{route('admin.testimonials.edit', $testimonial)}}" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+                        <a href="{{route('admin.testimonials.edit', $testimonial)}} " class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
                             Edit
                         </a>
-                        <form action="{{route('admin.testimonials.destroy', $testimonial)}}" method="POST">
+                        <form action="{{route('admin.testimonials.destroy', $testimonial)}} " method="POST">
                             @csrf
                             @method('DELETE')
-
                             <button type="submit" class="font-bold py-4 px-6 bg-red-700 text-white rounded-full">
                                 Delete
                             </button>
@@ -45,8 +40,10 @@
                     </div>
                 </div>
                 @empty
-                <p>belum ada data terbaru</p>
+                <p>belum ada data</p>
                 @endforelse
+
+                
             </div>
         </div>
     </div>
